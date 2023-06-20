b905e357d22d02e288a43c2398ec9c09c0d71033
KS Kim <65766852+simeddk@users.noreply.github.com>
2023-06-20 19:27:39 +0900

    0620_PlayerInput

--- Source/U04_BasicSyntax/Chracters/CPlayer.cpp
+++ Source/U04_BasicSyntax/Chracters/CPlayer.cpp
@@ -4,6 +4,7 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "CAnimInstance.h"
 
 ACPlayer::ACPlayer()
 {
@@ -14,6 +15,7 @@ ACPlayer::ACPlayer()
 	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
 
 	//Component Settings
+	//<Mesh>
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> meshAsset(TEXT("SkeletalMesh'/Game/Character/Mesh/SK_Mannequin.SK_Mannequin'"));
 	if (meshAsset.Succeeded())
 		GetMesh()->SetSkeletalMesh(meshAsset.Object);
@@ -21,14 +23,21 @@ ACPlayer::ACPlayer()
 	GetMesh()->SetRelativeLocation(FVector(0, 0, -88));
 	GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
 
+	ConstructorHelpers::FClassFinder<UCAnimInstance> animClass(TEXT("AnimBlueprint'/Game/Player/ABP_CPlayer.ABP_CPlayer_C'"));
+	if (animClass.Succeeded())
+		GetMesh()->SetAnimInstanceClass(animClass.Class);
+
+	//<SpringArm>
 	SpringArm->SetupAttachment(GetCapsuleComponent());
 	SpringArm->TargetArmLength = 200.f;
 	SpringArm->bDoCollisionTest = false;
 	SpringArm->bUsePawnControlRotation = true;
-	//Todo. 스프링암 위치 조정
+	SpringArm->SetRelativeLocation(FVector(0, 0, 60));
 
+	//Cameera
 	Camera->SetupAttachment(SpringArm);
 
+	//Movement
 	bUseControllerRotationYaw = false;
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->MaxWalkSpeed = 400.f;
@@ -52,7 +61,11 @@ void ACPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ACPlayer::OnMoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ACPlayer::OnMoveRight);
+	PlayerInputComponent->BindAxis("HorizontalLook", this, &ACPlayer::OnHorizontalLook);
+	PlayerInputComponent->BindAxis("VerticalLook", this, &ACPlayer::OnVerticalLook);
 
+	PlayerInputComponent->BindAction("Sprint", EInputEvent::IE_Pressed, this, &ACPlayer::OnSprint);
+	PlayerInputComponent->BindAction("Sprint", EInputEvent::IE_Released, this, &ACPlayer::OffSprint);
 }
 
 void ACPlayer::OnMoveForward(float Axis)
@@ -69,4 +82,24 @@ void ACPlayer::OnMoveRight(float Axis)
 	FVector direction = FQuat(rotator).GetRightVector().GetSafeNormal2D();
 
 	AddMovementInput(direction, Axis);
+}
+
+void ACPlayer::OnHorizontalLook(float Axis)
+{
+	AddControllerYawInput(Axis);
+}
+
+void ACPlayer::OnVerticalLook(float Axis)
+{
+	AddControllerPitchInput(Axis);
+}
+
+void ACPlayer::OnSprint()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 600.f;
+}
+
+void ACPlayer::OffSprint()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
 }
